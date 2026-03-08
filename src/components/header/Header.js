f8bc511951167b90aecd68c6e3b9622b347334f1
Assane Dia <119739327+sannassD@users.noreply.github.com>
2026-03-08 05:19:01 +0000

    Shorten 'Contact Me' link text to 'Contact'

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -77,7 +77,7 @@ function Header() {
             </li>
           )}
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact">Contact</a>
           </li>
           <li>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
